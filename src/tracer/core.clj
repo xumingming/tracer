4e40996940eb2fc0adc271861deea9d8d18dbe8b
James Xu <xumingming64398966@gmail.com>
2013-01-16 01:39:42 +0800

    show the stack trace level

--- src/tracer/core.clj
+++ src/tracer/core.clj
@@ -1,6 +1,12 @@
 (ns tracer.core
   (require [clojure.string :refer [split]]))
 
+(def level (atom 0))
+
+(defn print-level []
+  (doseq [i (range @level)]
+    (print "  ")))
+
 (defn parse-ns-name [f]
   (let [full-class-name (-> f type .getName)
         [ns-name fn-name] (vec (.split full-class-name "\\$"))
@@ -14,11 +20,17 @@
                         (fn [& args#]
                           (let [[ns-name# fn-name#] (parse-ns-name  original#)
                                 display-fn-name# (str ns-name# "$" fn-name#)]
+                            (print-level)
+                            ;; incr the level
+                            (swap! level inc)
+                            (print-level)
                             (println "[TRACER]" display-fn-name#  args#)
                             (let [ret# (apply original# args#)]
+                              ;; decr the level
+                              (swap! level dec)
                               ret#))))))
 
-(defn wrap-ns [ns-name-sym]
+(defn trace [ns-name-sym]
   (let [vars (ns-interns ns-name-sym)]
      (doseq [[var-name var-obj] vars
              :let [var-name (name var-name)
