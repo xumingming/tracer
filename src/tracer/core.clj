2f38ffa9f2ce2572922bc7a98fd7c080d77084b8
Baishampayan Ghose <b.ghose@gmail.com>
2013-01-16 12:56:19 +0530

    Add a function `untrace` to undo all the tracing.

--- src/tracer/core.clj
+++ src/tracer/core.clj
@@ -21,18 +21,26 @@
   (not (nil? (:arglists (meta var-obj)))))
 
 (defmacro wrap-fn [f]
-  `(alter-var-root ~f (fn [original#]
-                        (fn [& args#]
-                          (let [[ns-name# fn-name#] (parse-ns-name  original#)
-                                display-fn-name# (str ns-name# "$" fn-name#)]
-                            (print-level)
-                            ;; incr the level
-                            (swap! level inc)
-                            (println display-fn-name#  args#)
-                            (let [ret# (apply original# args#)]
-                              ;; decr the level
-                              (swap! level dec)
-                              ret#))))))
+  `(do
+     (alter-meta! ~f assoc ::orig (deref ~f))
+     (alter-var-root ~f (fn [original#]
+                          (fn [& args#]
+                            (let [[ns-name# fn-name#] (parse-ns-name  original#)
+                                  display-fn-name# (str ns-name# "$" fn-name#)]
+                              (print-level)
+                              ;; incr the level
+                              (swap! level inc)
+                              (println display-fn-name#  args#)
+                              (let [ret# (apply original# args#)]
+                                ;; decr the level
+                                (swap! level dec)
+                                ret#)))))))
+
+(defn unwrap-fn [v]
+  (when (::orig (meta v))
+    (doto v
+      (alter-var-root (constantly ((meta v) ::orig)))
+      (alter-meta! dissoc ::orig))))
 
 (defn trace [ns-name-sym]
   (let [vars (ns-interns ns-name-sym)]
@@ -41,3 +49,7 @@
         (println "Add" var-name "to trace list.")
         (wrap-fn var-obj)))))
 
+(defn untrace [ns-name-sym]
+  (doseq [[_ var-obj] (ns-interns ns-name-sym)]
+    (when (callable? var-obj)
+      (unwrap-fn var-obj))))
